d8be4c576a3235f0a9658322a385ad2c729ed20a
YuuShigetani <s2g4t1n2@gmail.com>
2014-03-12 02:32:36 +0900

    Add cmake

---
+++ site-cookbooks/cmake/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: cmake
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'cmake' do
+  action :install
+end
