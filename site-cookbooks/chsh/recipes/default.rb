0c90dab87f2a0abe53dccb8a256a79a5bb9bb76e
YuuShigetani <s2g4t1n2@gmail.com>
2014-03-13 08:57:24 +0900

    fix chsh

--- site-cookbooks/chsh/recipes/default.rb
+++ site-cookbooks/chsh/recipes/default.rb
@@ -11,7 +11,6 @@ user = node['user']
 
 bash "Set vagrant's shell to zsh" do
   code <<-EOT
-    chsh -s $(which zsh) #{user}
+    chsh -s /bin/zsh #{user}
   EOT
-  not_if "test \"$(which zsh)\" = \"$(grep #{user} /etc/passwd | cut -d: -f7)\""
 end
