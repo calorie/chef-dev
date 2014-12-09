49e6039ee3abe5fdde14f09b17e31b0962196aa1
YuuShigetani <s2g4t1n2@gmail.com>
2014-12-10 00:44:14 +0900

    Bump up version of packer

--- site-cookbooks/packer/attributes/default.rb
+++ site-cookbooks/packer/attributes/default.rb
@@ -1,2 +1,2 @@
-default['packer']['version'] = '0.7.1'
+default['packer']['version'] = '0.7.2'
 default['packer']['url'] = "https://dl.bintray.com/mitchellh/packer/packer_#{node['packer']['version']}_linux_amd64.zip"
