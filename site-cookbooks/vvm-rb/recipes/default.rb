30955ce33c1774466cc54b4ab4620cb438e5d743
YuuShigetani <s2g4t1n2@gmail.com>
2015-03-30 05:59:41 +0900

    fix bug

--- site-cookbooks/vvm-rb/recipes/default.rb
+++ site-cookbooks/vvm-rb/recipes/default.rb
@@ -14,11 +14,13 @@ opts    = node['vvm-rb']['opts'].join(' ')
 
 Array(node['rbenv']['user_installs']).each do |rbenv_user|
   next unless rbenv_user['gems'][rbenv_user['global']].map { |h| h['name'] }.include?('vvm-rb')
+  home = ::File.join(node['rbenv']['user_home_root'], rbenv_user['user'])
   rbenv_script "vvm-rb install #{version}" do
     rbenv_version rbenv_user['global']
     user          rbenv_user['user']
     group         rbenv_user['group']
+    cwd           home
     code          "vvm install -u #{version} #{opts}"
-    not_if        { File.exist?(File.join(node['rbenv']['user_home_root'], rbenv_user['user'], '.vvm-rb')) }
+    not_if        { File.exist?(File.join(home, '.vvm-rb')) }
   end
 end
